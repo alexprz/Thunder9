389616814dbc5a7e9694628a19f0867a37cd480b
Antoine Eudes <antoine@eudes.co>
2018-02-16 14:18:35 +0100

    moved things to Trash

--- Thunder9/main.cpp
+++ Thunder9/main.cpp
@@ -6,13 +6,7 @@
 //  Copyright © 2018 Alexandre PEREZ. All rights reserved.
 //
 
-//#include <iostream>
-//
-//int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    std::cout << "Hello, World!\n";
-//    return 0;
-//}
+
 #include <iostream>
 
 using namespace std;
@@ -56,35 +50,3 @@ int main()
     t1.join();
     return 0;
 }
-
-//int main(){
-//    if (sf::SoundBufferRecorder::isAvailable())
-//    {
-//        cout << "Avaiable";
-//        // Record some audio data
-//        sf::SoundBufferRecorder recorder;
-//        recorder.start();
-//        for(int i=0; i< 100000; i++)
-//        {
-//            cout << i << endl;
-//        }
-//        recorder.stop();
-////         Get the buffer containing the captured audio data
-//        const sf::SoundBuffer& buffer = recorder.getBuffer();
-////         Save it to a file (for example...)
-////        cout << buffer.getDuration();
-//        const sf::Int16* samples = buffer.getSamples();
-//        std::size_t count = buffer.getSampleCount();
-//        cout << samples << endl << count << endl << endl;
-//        for (int j=0; j<count; j++)
-//        {
-//            cout << samples[j] << endl;
-//
-//        }
-//    }
-//    else
-//        cout << "Not avaiable" << endl;
-//    cout << "Hello world !";
-//    return 0;
-//}
-
